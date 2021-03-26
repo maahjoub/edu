8fdc2b3c8a2a7fdb56d0fc870a6490f84cfcce2d
mahjoub <am470593@gmail.com>
2021-03-26 14:33:07 +0200

    new uoddate

--- js/main.js
+++ js/main.js
@@ -9,17 +9,7 @@ window.onscroll = function() {
         nav.style.top = '35px';
           } else {
             topoHeader.className = "top-header";
-            topoHeader.style.position = '';
+            topoHeader.style.position = 'sticky';
             nav.style.top = '135px';
           }
 };
-
-// window.onscroll = function() {myFunction()};
-
-// function myFunction() {
-//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//     document.getElementById("myP").className = "test";
-//   } else {
-//     document.getElementById("myP").className = "";
-//   }
-// }
